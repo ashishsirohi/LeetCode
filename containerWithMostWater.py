2fd6b4182b8db2cc2890a416502c01ada32f6a6c
ashishsirohi <ashishsirohi@live.com>
2017-09-13 01:29:54 -0700

    faster solution to water container problem

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -3,7 +3,7 @@ def maxArea(height):
     :type height: List[int]
     :rtype: int
     """
-    maxAr = 0
+    """maxAr = 0
     x = 0
     while x < len(height):
         y = x + 1
@@ -20,6 +20,23 @@ def maxArea(height):
             y = y + 1
         x = x + 1
 
-    return maxAr
+    return maxAr"""
+	maxArea = 0
+    x = 0 
+    y = len(height) - 1
+        
+    while x < y:
+        area = 0
+        if height[x] < height[y]:
+            area = (y-x) * (height[x])
+            x = x + 1
+        else:
+            area = (y-x) * (height[y])
+            y = y - 1
+            
+        if area > maxArea:
+            maxArea = area
+                    
+    return maxArea
 
 print maxArea([1,2,4,3])
